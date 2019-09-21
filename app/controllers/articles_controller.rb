a2cca460e559837786834a3528e6679d658fcd12
EitanFuturo <llovia@gmail.com>
2019-09-21 16:26:40 -0300

    Some little view refactoring.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,10 +49,9 @@ class ArticlesController < ApplicationController
 
   def admin_view
     @user_groups = UserGroup.all
-    @users = User.order(:email)
+
     # TODO: should be replaced with actual dates that the Admin should select
     @week_days = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
-    render 'admin_view'
   end
 
   private
