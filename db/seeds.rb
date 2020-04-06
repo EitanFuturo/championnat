d5686e864d7f41a172168882ec8deb7bcc105b71
EitanFuturo <llovia@gmail.com>
2020-04-05 22:11:34 -0300

    Se agrega un usuario al seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,16 @@
 #championship_types = ChampionshipType.create([{ name: 'Prosa', length: 7, min_characters: 3000 }, { name: 'Poesía', length: 7 }])
 #Championship.create([{ start_date: Date.today, championship_type: championship_types.first }, { start_date: Date.today + 7, championship_type: championship_types.last }])
 
-group = UserGroup.create(name: 'Desastre Anunciado')
-
-User.create([{ email: 'magalietchebarne@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Maga'},
-             { email: 'lamarieva@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Evi'},
-             { email: 'rafaelotegui@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Rafa'},
-             { email: 'llovia@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', admin: true, name: 'Eiti'},
-             { email: 'jpoquelin@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Dam'},
-             { email: 'sgarciauldry@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Seba'},
-             { email: 'majomoiron@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Majo'},
-             { email: 'lautaro.lamisovski@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Lauti'},
-             { email: 'santiago.llach@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Santi'},
-             { email: 'pablo_ottonello@yahoo.com.ar', user_group: group, password: '123456', password_confirmation: '123456', name: 'Otto'},
-])
+group = UserGroup.find_by(name: 'Desastre Anunciado')
+User.create(email: 'julietamortati@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Juli')
+#User.create([{ email: 'magalietchebarne@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Maga'},
+#             { email: 'lamarieva@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Evi'},
+#             { email: 'rafaelotegui@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Rafa'},
+#             { email: 'llovia@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', admin: true, name: 'Eiti'},
+#             { email: 'jpoquelin@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Dam'},
+#             { email: 'sgarciauldry@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Seba'},
+#             { email: 'majomoiron@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Majo'},
+#             { email: 'lautaro.lamisovski@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Lauti'},
+#             { email: 'santiago.llach@gmail.com', user_group: group, password: '123456', password_confirmation: '123456', name: 'Santi'},
+#             { email: 'pablo_ottonello@yahoo.com.ar', user_group: group, password: '123456', password_confirmation: '123456', name: 'Otto'},
+#])
